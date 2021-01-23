7052fe1e2f53d09ecc657755fa4c51347e507b14
melikebilen <bilenmelike@gmail.com>
2021-01-23 17:44:06 +0300

    New Commit

---
+++ .history/index_20210116215547.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const firebase = require("firebase/app");
+require('firebase/auth');
+require('firebase/database');
+//const functions = require('firebase-functions');
+//const admin = require('firebase-admin');
+const app = express();
+
+const PORT = process.env.PORT || 2000; //the reason we do it is for deployment
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBQjxxJqCcYhjYDH6GFwjSGdQ7VeqVGhCI",
+    authDomain: "tdoa-23cf7.firebaseapp.com",
+    databaseURL: "https://tdoa-23cf7-default-rtdb.firebaseio.com",
+    projectId: "tdoa-23cf7",
+    storageBucket: "tdoa-23cf7.appspot.com",
+    messagingSenderId: "611153024504",
+    appId: "1:611153024504:web:0f5e82fbf77d611aaf720d",
+    measurementId: "G-YYER7FY9DQ"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+//var database = firebase.database();
+
+
+  var countData;
+  var count = firebase.database().ref('Count');
+  count.on('value', (snapshot) => {
+    countData = snapshot.val();
+    console.log(countData);
+  });
+
+  var allData = firebase.database().ref();
+  allData.on('value', (snapshot) => {
+    const locationData = snapshot.val();
+    console.log(locationData);
+  }); 
+
+  var location = firebase.database().ref('Location1');
+  allData.on('value', (snapshot) => {
+    const locationData = snapshot.val();
+    console.log(locationData);
+  }); 
+
+app.get('/' ,(req,res)=>{
+    res.render('index',  { count: countData.count , location : allData[1]});
+});
+
+app.set('view engine','ejs');
+
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
+
+module.exports = app;
+
+
+
+//npm run dev ile çalıştırıyoruz, nodemon, express indirdik
+// takip ettiğim video : https://www.youtube.com/watch?v=L72fhGm1tfE&list=PLBrS5D_YHVCjwpc3zaepc4nbGr6tCLQHl&index=3
---
+++ .history/index_20210117184303.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const firebase = require("firebase/app");
+require('firebase/auth');
+require('firebase/database');
+//const functions = require('firebase-functions');
+//const admin = require('firebase-admin');
+const app = express();
+
+const PORT = process.env.PORT || 2000; //the reason we do it is for deployment
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBQjxxJqCcYhjYDH6GFwjSGdQ7VeqVGhCI",
+    authDomain: "tdoa-23cf7.firebaseapp.com",
+    databaseURL: "https://tdoa-23cf7-default-rtdb.firebaseio.com",
+    projectId: "tdoa-23cf7",
+    storageBucket: "tdoa-23cf7.appspot.com",
+    messagingSenderId: "611153024504",
+    appId: "1:611153024504:web:0f5e82fbf77d611aaf720d",
+    measurementId: "G-YYER7FY9DQ"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+//var database = firebase.database();
+
+
+  var countData;
+  var count = firebase.database().ref('Count');
+  count.on('value', (snapshot) => {
+    countData = snapshot.val();
+    console.log(countData);
+  });
+  var locationData;
+  var allData = firebase.database().ref();
+  allData.on('value', (snapshot) => {
+    locationData = snapshot.val();
+    console.log(locationData);
+  }); 
+
+  var list = [locationData[0],'b','c'];
+
+app.get('/' ,(req,res)=>{
+    res.render('index',  {list:list,location: locationData, count: countData.count});
+});
+
+app.set('view engine','ejs');
+
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
+
+module.exports = app;
+
+
+
+//npm run dev ile çalıştırıyoruz, nodemon, express indirdik
+// takip ettiğim video : https://www.youtube.com/watch?v=L72fhGm1tfE&list=PLBrS5D_YHVCjwpc3zaepc4nbGr6tCLQHl&index=3
